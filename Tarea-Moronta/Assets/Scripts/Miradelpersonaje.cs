0464685f0cf95c265c562e4287fd1e1dee329267
ThElemenet12 <45013990+ThElemenet12@users.noreply.github.com>
2019-02-08 15:54:19 -0400

    klk

---
+++ Tarea-Moronta/Assets/Scripts/Miradelpersonaje.cs
@@ -0,0 +1,50 @@
+﻿-﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miradelpersonaje : MonoBehaviour
+{
+    float _ANGULO = 0;
+
+    enum Estado
+    {
+        subiendo,
+        bajando
+    }
+
+    Estado estado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        estado = Estado.subiendo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 0, _ANGULO));
+        if (gameObject.name == "Articulacion 1")
+        {
+
+            if (estado == Estado.subiendo && gameObject.transform.eulerAngles.z >= -90f)
+            {
+                _ANGULO -= 0.05f;
+                if (gameObject.transform.eulerAngles.z <= -90f)
+                {
+                    estado = Estado.bajando;
+                }
+
+            }
+            if (estado == Estado.bajando && gameObject.transform.eulerAngles.z <= 0f)
+            {
+                _ANGULO += 0.05f;
+                if (gameObject.transform.eulerAngles.z >= 0)
+                {
+                    estado = Estado.subiendo;
+                }
+            }
+
+        }
+    }
+}
