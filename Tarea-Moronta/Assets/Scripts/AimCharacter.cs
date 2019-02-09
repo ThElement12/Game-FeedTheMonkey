d4e3e38bcc5e81bbb3716113c999d1df146436df
ThElemenet12 <45013990+ThElemenet12@users.noreply.github.com>
2019-02-09 10:33:06 -0400

    Par de problemas resueltos

--- Tarea-Moronta/Assets/Scripts/AimCharacter.cs
+++ Tarea-Moronta/Assets/Scripts/AimCharacter.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class AimCharacter : MonoBehaviour
 {
     float _VELOCIDAD = 50;
-    public float BSpeedX = 4f;
-    public float BSpeedY = 4f;
-    GameObject banana;
-    public GameObject proyectil;
-    GameObject miPadre;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,20 +19,7 @@ public class AimCharacter : MonoBehaviour
         gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 0,
         Input.GetAxis("Vertical")) * _VELOCIDAD * Time.deltaTime * (gameObject.name == "Articulacion 1" ? -1 : 1));
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Disparar();
-        }
     }
 
-    public void Disparar()
-    {
-        miPadre = GameObject.Find("Brazo 1");
-        banana = Instantiate(proyectil, new Vector3(miPadre.transform.position.x, miPadre.transform.position.y, miPadre.transform.position.z), miPadre.transform.rotation);
-        //banana.transform.SetParent(miPadre.transform);
 
-        banana.transform.parent = null;
-        banana.GetComponent<Rigidbody>().AddForce( BSpeedX* gameObject.transform.position.x * -1,
-            BSpeedY* gameObject.transform.position.y * -1, gameObject.transform.position.z*-1, ForceMode.Impulse);
-    }
 }
--- Tarea-Moronta/Assets/Scripts/Disparar.cs
+++ Tarea-Moronta/Assets/Scripts/Disparar.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Disparar : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float BSpeedX = 4f;
+    public float BSpeedY = 4f;
+    GameObject banana;
+    public GameObject proyectil;
+    GameObject miPadre;
+
     void Start()
     {
         
@@ -13,6 +20,24 @@ public class Disparar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            disparar();
+        }
+    }
+
+    public void disparar()
+    {
+        miPadre = GameObject.Find("Disparador");
+        banana = Instantiate(proyectil, new Vector3(miPadre.transform.position.x, miPadre.transform.position.y, miPadre.transform.position.z), miPadre.transform.rotation);
+        //banana.transform.SetParent(miPadre.transform);
+
+        //Vector3 dir = Quaternion.AngleAxis(miPadre.transform.rotation.z, Vector3.forward) * Vector3.right;
+
+
+        banana.GetComponent<Rigidbody>().AddForce(BSpeedX* gameObject.transform.position.x * -1, BSpeedY* gameObject.transform.position.y * 45, gameObject.transform.position.z, ForceMode.Impulse);
+       /* banana.GetComponent<Rigidbody>().AddForce(Quaternion.AngleAxis(miPadre.transform.rotation.z, new Vector3(miPadre.transform.localPosition.x, miPadre.transform.localPosition.y)) * new Vector3(gameObject.transform.position.x * -1, gameObject.transform.position.y * -1)
+        , ForceMode.Impulse);*/
+        banana.transform.parent = null;
     }
 }
