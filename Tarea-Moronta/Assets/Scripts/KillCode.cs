2955bb091af368200bbf9668af05499bb117733a
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2019-02-09 16:52:38 -0400

    algunos cambios
    ok?

---
+++ Tarea-Moronta/Assets/Scripts/KillCode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillCode : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Banana")
+        {
+            Destroy(other.gameObject);
+            
+        }
+    }
+}
