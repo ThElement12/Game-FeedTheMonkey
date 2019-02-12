6a0c2f7ed87512c307c5910ef9f05aedf4e7c141
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2019-02-11 22:13:14 -0400

    pusheate

---
+++ Tarea-Moronta/Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroll : MonoBehaviour
+{
+    public GameObject mapa1;
+    public GameObject mapa2;
+    public GameObject mapa3;
+    public GameObject personaje;
+    GameObject nextmap;
+    CentroJuegos centro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextmap = Instantiate(mapa1);
+        //firstmap.SetActive(false);
+        nextmap.transform.position = new Vector3(mapa1.transform.position.x - 19.422f, mapa1.transform.position.y + 4.08948f);
+        mapa1 = nextmap;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Tarea-Moronta/Assets/Scripts/CentroJuegos.cs
+++ Tarea-Moronta/Assets/Scripts/CentroJuegos.cs
@@ -19,6 +19,13 @@ public class CentroJuegos : MonoBehaviour
     Disparar disparador;
     AudioSource audio;
     
+    public GameObject mapa1;
+    public GameObject mapa2;
+    public GameObject mapa3;
+    public GameObject personaje;
+    public GameObject enemy;
+    GameObject nextmap;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,12 +56,16 @@ public class CentroJuegos : MonoBehaviour
 
                 if (!acertado && !bananaAlive)
                 {
-
                     turno = eTurno.TurnoMaquina;
                 }
                 else
                 {
                     puntajeJugador++;
+                    nextmap = Instantiate(mapa1);
+                    nextmap.SetActive(true);
+                    nextmap.transform.position = new Vector3(mapa1.transform.position.x - 19.422f, mapa1.transform.position.y + 4.08948f);
+                    turno = eTurno.TurnoJugador;
+                    mapa1 = nextmap;
                 }
                 break;
             case eTurno.TurnoMaquina:
