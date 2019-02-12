8675465f9cdc883be8ed4a54f07d103e0e0a4939
ThElemenet12 <45013990+ThElemenet12@users.noreply.github.com>
2019-02-12 14:43:56 -0400

    Update AimCharacter
    Ya se mueve solo

--- Tarea-Moronta/Assets/Scripts/AimCharacter.cs
+++ Tarea-Moronta/Assets/Scripts/AimCharacter.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class AimCharacter : MonoBehaviour
 {
-    float _VELOCIDAD = 50;
-    
+    float _VELOCIDAD = 1f;
+    public float periodo = 150;
+    public float amplitud = 100;
+    float VELOCIDADANGULAR;
+    float angulo;
+    float variacion;
 
     // Start is called before the first frame update
     void Start()
     {
-
+       VELOCIDADANGULAR = (Mathf.PI / periodo) * 5;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 0,
-        Input.GetAxis("Vertical")) * _VELOCIDAD * Time.deltaTime * (gameObject.name == "Articulacion 1" ? -1 : 1));
+        angulo += (2 * Mathf.PI) / periodo;
 
+        variacion = amplitud * Mathf.Sin(angulo);
+
+        if(gameObject.transform.rotation.z < 90)
+        {
+            gameObject.GetComponent<Transform>().Rotate(Vector3.forward, -variacion * Time.deltaTime * _VELOCIDAD);
+        }
+        else
+        {
+            gameObject.GetComponent<Transform>().Rotate(Vector3.forward, variacion * Time.deltaTime * _VELOCIDAD);
+        }
+        
+  //      gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 0,
+//        Input.GetAxis("Vertical")) * _VELOCIDAD * Time.deltaTime * (gameObject.name == "Articulacion 1" ? -1 : 1));
+        
     }
 
 
